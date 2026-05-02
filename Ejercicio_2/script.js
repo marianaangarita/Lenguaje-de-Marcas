2cbf3a7f60e54581c67fb727af82f7ac4407e22b
marianaangarita <meangarita@alumnes.santjosepobrer.es>
2026-05-02 20:13:33 +0200

    progreso con el ejercicio 2

--- Ejercicio_2/script.js
+++ Ejercicio_2/script.js
@@ -1,11 +1,36 @@
 let alumnos = [];
 
 async function cargarAlumnos() {
+    const response = await fetch("alumnos.json");
+    const datos = await response.json();
 
+    alumnos = datos.alumnos;
+    mostrarAlumnos(alumnos);
 }
 
 function mostrarAlumnos(lista) {
+    let contenedor = document.getElementById("tabla-alumnos");
 
+    contenedor.textContent = "";
+
+    lista.forEach(unAlumno => {
+        contenedor.innerHTML += `
+        <tr>
+            <td>
+                <b>Nombre: </b> ${unAlumno.nombre}
+            </td>
+            <td>
+                <b>Edad: </b> ${unAlumno.edad}
+            </td>
+            <td>
+                <b>Curso: </b> ${unAlumno.curso}
+            </td>
+            <td>
+                <b>Nota Media: </b> ${unAlumno.notaMedia}
+            </td>
+        </tr>
+        `
+    });
 }
 
 document.getElementById("btnTodos").addEventListener("click", () => {
