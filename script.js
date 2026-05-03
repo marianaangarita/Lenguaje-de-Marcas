a6fba11c9b342aeb507eebbdd677f429f86e6b2d
marianaangarita <meangarita@alumnes.santjosepobrer.es>
2026-05-03 21:30:41 +0200

    primera parte del trabajo hecho

--- script.js
+++ script.js
@@ -39,6 +39,84 @@ function cancionesTendencia(lista){
     });
 }
 
+function artistasPopulares(lista){
+    const artistas = document.getElementById("contenedor-artistas");
+
+    lista.forEach(artista => {
+
+        artistas.innerHTML += `
+            <div class="brilloGris">
+                <img class="flechaVerde" src="svg/flechaVerde.svg" alt="flechaVerde">
+                <img src=${artista.srcPopular} alt="portada de música">
+                <h3 class="textDecoration">${artista.nombreArtistaPopular}</h3>
+                <p>Artista</p>
+            </div>
+        `
+    });
+
+}
+
+function albumesPopulares(lista){
+    const albumnes = document.getElementById("contenedor-albumnes");
+
+    lista.forEach(album => {
+        if (Array.isArray(album.nombreArtista)){
+
+            albumnes.innerHTML += `
+                <div class="brilloGris">
+                    <img class="flechaVerde" src="svg/flechaVerde.svg" alt="flechaVerde">
+                    <img src=${album.srcAlbum} alt="portada de música">
+                    <h3 class="textDecoration">${album.titulo}</h3>
+                    <p class="textDecoration">${album.nombreArtista.join(", ")}</p>
+                </div>
+            `
+        }else{
+            albumnes.innerHTML += `
+                <div class="brilloGris">
+                    <img class="flechaVerde" src="svg/flechaVerde.svg" alt="flechaVerde">
+                    <img src=${album.srcAlbum} alt="portada de música">
+                    <h3 class="textDecoration">${album.titulo}</h3>
+                    <p class="textDecoration">${album.nombreArtista}</p>
+                </div>
+            `
+        }
+    });
+}
+function emisorasPopulares(lista){
+    const emisoras = document.getElementById("emisoras-populares");
+
+    lista.forEach(emisora => {
+
+        emisoras.innerHTML += `
+            <div class="brilloGris">
+                <img class="flechaVerde" src="svg/flechaVerde.svg" alt="flechaVerde">
+                <img src=${emisora.scrEmisoras} alt="portada de música">
+                    <div>
+                        ${emisora.artistas.map(artista => `<p>${artista}</p>`).join("")}
+                    </div>
+            </div>
+        
+        `
+    });
+}
+function listasSeleccionadas(lista){
+    const listasSeleccionadas = document.getElementById("listas-seleccionadas");
+
+    lista.forEach(lisSelec => {
+
+        listasSeleccionadas.innerHTML += `
+           <div class="brilloGris">
+                <img class="flechaVerde" src="svg/flechaVerde.svg" alt="flechaVerde">
+                <img src=${lisSelec.srcListas} alt="portada de música">
+                    <div>
+                        ${lisSelec.titulo.map(titulo => `<p>${titulo}</p>`).join("")}
+                    </div>
+
+            </div> 
+        `
+    });
+}
+
 
 async function cargarCanciones() {
     try{
@@ -47,6 +125,18 @@ async function cargarCanciones() {
 
         cancionesDatos = datos.cancionesTendencia;
         cancionesTendencia(cancionesDatos);
+
+        artistasPopularesDatos = datos.artistasPopulares;
+        artistasPopulares(artistasPopularesDatos);
+
+        albumesPopularesDatos = datos.albumesPopulares;
+        albumesPopulares(albumesPopularesDatos);
+
+        emisorasPopularesDatos = datos.emisorasPopulares;
+        emisorasPopulares(emisorasPopularesDatos);
+
+        listasSeleccionadasDatos = datos.listasSeleccionadas;
+        listasSeleccionadas(listasSeleccionadasDatos);
 
     } catch (error){
         console.error("Hubo un error cargando el JSON:", error);
